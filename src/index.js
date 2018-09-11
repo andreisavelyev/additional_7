79d6059bcfe740e7c450db1160d56daa8a66e16b
Andrei <36643486+andreisavelyev@users.noreply.github.com>
2018-09-11 21:01:01 +0300

    fix emptyPositions

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ module.exports = function solveSudoku(matrix) {
 
   for (var i = 0; i < 9; i++){
     for (var j = 0; j < 9; j++){
-      if(matrix[i][j]===0);
-      emptyPositions.push([i,j])
+      if(matrix[i][j]===0) {
+        emptyPositions.push([i,j])
+      }
     }
   }
 
